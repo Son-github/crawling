616cccb3d5eec12faeeb93bfc394fbf8a161eeca
SonJongIN <jongin718@gmail.com>
2023-03-28 00:44:29 +0900

    Default Changelist

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import openpyxl
-
-
-excel_file = openpyxl.Workbook()
-excel_sheet = excel_file.active
+import pandas as pd
 
 #driver = webdriver.Chrome(ChromeDriverManager().install()) #mac 전용
 
@@ -23,10 +19,6 @@ driver.find_element(By.XPATH,'//*[@id="container"]/form/div/div/div/iframe').cli
 sleep(10)
 driver.find_element(By.XPATH,'//*[@id="container"]/form/p[3]/input').click()
 
-#전체검색어에 키워드 입력
-driver.find_element(By.CSS_SELECTOR,"#container > div.rightside > form > input").send_keys("자취")
-driver.find_element(By.CSS_SELECTOR,"#container > div.rightside > form > input").send_keys(Keys.ENTER)
-
 #크롤링 함수
 def next_page():
     current = driver.current_url
@@ -37,6 +29,7 @@ def next_page():
     data_url_list = []
     for i in range(len(data_url)):
         data_url_list.append('https://everytime.kr' + data_url[i].get('href')) #url을 하이퍼링크로 전환하여 data_url_list에 넣기
+        url_list.append('https://everytime.kr' + data_url[i].get('href'))
 
     for i in data_url_list:
         driver.get(i) #data_url_list안에 있는 하이퍼링크를 차례로 방문
@@ -47,53 +40,93 @@ def next_page():
 
         if comment_num == 0: #댓글의 갯수가 0일 때
             if len(name) == 0: #소제목이 없을 때
-                excel_sheet.append(
-                    [comment_num, " ", text, i, " ", comment_time]) #엑셀 시트에 댓글 갯수, 빈칸, 내용, 하이퍼링크를 추가
+                comment_num_list.append(comment_num)
+                name_list.append(" ")
+                text_list.append(text)
+                comment_time_list.append(comment_time)
+                comment_list.append(" ")
+
             else:
-                excel_sheet.append(
-                    [comment_num, name[0].text, text, i, " " ,comment_time]) #엑셀 시트에 댓글 갯수, 제목, 내용, 하이퍼링크를 추가
+                comment_num_list.append(comment_num)
+                name_list.append(name[0].text)
+                text_list.append(text)
+                comment_time_list.append(comment_time)
+                comment_list.append(" ")
+
         elif comment_num == 1: #댓글의 갯수가 1일 때
             try:
-                comment = driver.find_element(By.CSS_SELECTOR, 'p.large').text #댓글 크롤링
+                comment = driver.find_element(By.CSS_SELECTOR, '#container > div.wrap.articles > article > div > article > p').text #댓글 크롤링
+                a = []
                 if len(name) == 0: #소제목이 없을 때
-                    excel_sheet.append(
-                        [comment_num, " ", text, i, comment, comment_time]) #엑셀 시트에 댓글 갯수, 빈칸, 내용, 하이퍼링크, 댓글을 추가
+                    a.append(comment)
+                    comment_num_list.append(comment_num)
+                    name_list.append(" ")
+                    text_list.append(text)
+                    comment_time_list.append(comment_time)
+                    comment_list.append(a)
+
                 else:
-                    excel_sheet.append(
-                        [comment_num, name[0].text, text, i, comment, comment_time]) #엑셀 시트에 댓글 갯수, 제목, 내용, 하이퍼링크, 댓글을 추가
+                    a.append(comment)
+                    comment_num_list.append(comment_num)
+                    name_list.append(name[0].text)
+                    text_list.append(text)
+                    comment_time_list.append(comment_time)
+                    comment_list.append(a)
+
             except:
                 pass
         elif comment_num > 1: #댓글의 갯수가 1이상 일 때
             try:
-                comments = driver.find_elements(By.CSS_SELECTOR, 'p.large') #댓글들을 list형태로 크롤링
+                comments = driver.find_elements(By.CSS_SELECTOR, '#container > div.wrap.articles > article > div > article > p') #댓글들을 list형태로 크롤링
                 if len(name) == 0: #소제목이 없을 때
-                    for j in comments: #댓글을 차례대로 append
-                        excel_sheet.append(
-                            [comment_num, " ", text, i, j.text, comment_time]) #엑셀 시트에 댓글 갯수, 빈칸, 내용, 하이퍼링크, 댓글을 추가
+                    a = []
+                    for j in range(len(comments)):  # 댓글을 차례대로 append
+                        a.append(comments[j].text)
+                    comment_num_list.append(comment_num)
+                    name_list.append(" ")
+                    text_list.append(text)
+                    comment_time_list.append(comment_time)
+                    comment_list.append(a)
+
                 else:
-                    for j in comments: #댓글을 차례대로 append
-                        excel_sheet.append(
-                            [comment_num, name[0].text, text, i, j.text, comment_time]) #엑셀 시트에 댓글 갯수, 제목, 내용, 하이퍼링크, 댓글을 추가]
+                    a = []
+                    for j in range(len(comments)): #댓글을 차례대로 append
+                        a.append(comments[j].text)
+                    comment_num_list.append(comment_num)
+                    name_list.append(name[0].text)
+                    text_list.append(text)
+                    comment_time_list.append(comment_time)
+                    comment_list.append(a)
+
             except:
                 pass
     driver.get(current)
-    '''for i in range(len(data_url_list)):
-        driver.back() #하이퍼링크 방문 수만큼 뒤로'''
     driver.find_element(By.CSS_SELECTOR, 'a.next').click()
 
-for _ in range(5):
-    next_page()
-    if len(driver.find_elements(By.CSS_SELECTOR, 'a.next')) == 0:
-        break
+keywords = ["자취", "취두부"]
 
-'''while True:
-    if len(driver.find_elements(By.CSS_SELECTOR, 'a.next')) == 0:
-        break
-    else:
-        next_page()''' #이거 키워드 끝까지 추출하는 코드이니 각주 삭제 하지마ㅎㅎ
-
-excel_file.save('everytime_crawling.xlsx')
-# everytime crawling 라는 엑셀 파일에 저장
-excel_file.close()
-
+for i in keywords:
+    comment_num_list = []
+    name_list = []
+    text_list = []
+    comment_time_list = []
+    comment_list = []
+    url_list = []
+    #전체검색어에 키워드 입력
+    driver.find_element(By.CSS_SELECTOR,"#container > div.rightside > form > input").send_keys(i)
+    driver.find_element(By.CSS_SELECTOR,"#container > div.rightside > form > input").send_keys(Keys.ENTER)
+    for _ in range(2):
+        next_page()
+        if len(driver.find_elements(By.CSS_SELECTOR, 'a.next')) == 0:
+            break
+    df = pd.DataFrame({
+        'comment_num' : comment_num_list,
+        'title' : name_list,
+        'body' : text_list,
+        'url' : url_list,
+        'comment' : comment_list,
+        'comment_time' : comment_time_list
+    })
+    df.to_csv("everytime_crawling" + "_"+ i + ".csv", encoding='utf-8-sig')
 driver.quit()
+
